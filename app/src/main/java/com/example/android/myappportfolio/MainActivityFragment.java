cc67ba7b057acbe221b2f23c9c85806bcc847235
Raz3r <kokotekilata@gmail.com>
2015-06-09 01:42:32 +0100

    Created an empty project with FrameLayout. Setting up a listView to add the dummy app buttons

--- app/src/main/java/com/example/android/myappportfolio/MainActivityFragment.java
+++ app/src/main/java/com/example/android/myappportfolio/MainActivityFragment.java
@@ -9,14 +9,20 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment
+{
 
-    public MainActivityFragment() {
+    public MainActivityFragment()
+    {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+                             Bundle savedInstanceState)
+    {
+        View rootView=  inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        return rootView;
     }
 }
